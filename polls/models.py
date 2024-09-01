7a2ce88c28e92a09e0965482f17fca4c892f156f
Panida Rumriankit <146973594+PanidaRumriankit@users.noreply.github.com>
2024-09-01 21:17:09 +0700

    create 2 methods in Questions

--- polls/models.py
+++ polls/models.py
@@ -4,20 +4,39 @@ from django.db import models
 from django.utils import timezone
 
 
+def get_time_current():
+    """Return the current local time"""
+    return timezone.now()
+
+
 class Question(models.Model):
     """
     Create a question
     use question_text for a question and pub_date for a publishing date
     """
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField("date published", default=get_time_current)
+    end_date = models.DateTimeField("end date for voting", null=True, blank=True)
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
+        """Return True if the question was published lately"""
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        """Return True if the current local time is >= the publication date"""
+        now = timezone.now()
+        return now >= self.pub_date
+
+    def can_vote(self):
+        """Return True if voting is allowed for this question"""
+        now = timezone.now()
+        if self.end_date is None:
+            return now >= self.pub_date
+        return self.pub_date <= now <= self.end_date
 
 
 class Choice(models.Model):
