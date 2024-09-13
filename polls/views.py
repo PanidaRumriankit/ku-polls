2b78ff6adfd7aab490a6f43c7476877fc12f426e
PanidaRumriankit <panida.rum@ku.th>
2024-09-14 01:08:26 +0700

    Cahnge how to display questions on index

--- polls/views.py
+++ polls/views.py
@@ -25,10 +25,10 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-        Return the last five published questions (not including those set to be
+        Return the published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
