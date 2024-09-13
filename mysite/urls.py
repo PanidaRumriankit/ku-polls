28c4b2b3158f78e9d3709a448a1cb65948220297
PanidaRumriankit <panida.rum@ku.th>
2024-09-13 10:32:35 +0700

    update migrations

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,7 +22,7 @@ from polls import views
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-    path('', RedirectView.as_view(url='/polls/', permanent=True)),
+    path('', RedirectView.as_view(url='/polls/')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', views.signup, name='signup'),
 ]
