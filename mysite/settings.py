955d837c70b7764dfffe9177939b7e2c7744992a
PanidaRumriankit <panida.rum@ku.th>
2024-09-16 21:48:29 +0700

    change setting for css

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -120,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static_files'
 STATIC_URL = 'static/'
 
 # Default primary key field type
